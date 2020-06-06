05f6c3a5b9125caf4682873ed29e783c25a54c1f
Dmitry <zloybreg@gmail.com>
2020-06-07 01:34:31 +0700

    include movies url

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django_movie import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('movies.urls')),
 ]
 
 if settings.DEBUG:
